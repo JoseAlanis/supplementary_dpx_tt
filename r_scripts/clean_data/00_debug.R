f62efbb0ec4295adfb59c2124fd29584c2bebd85
Jose Alanis <joialanisson@gmail.com>
2018-06-13 14:05:38 +0200

    n segments

---
+++ r_scripts/clean_data/00_debug.R
@@ -0,0 +1,27 @@
+##### ##### #####     Analysis scrips for Alanis et al., 2018   ##### ##### #####
+#                          Load EGG data for analysis
+#
+
+setwd('~/Documents/Experiments/dpx_tt/')
+
+# ----- DEBUG number of segments per person
+for (i in 1:32) {
+  
+  if (!exists('temp')) {
+    temp <- data.frame(NA, NA)
+    names(temp) <- c('Subject', 'Segments')
+  }
+
+  temp[i, 1] <- i
+  temp[i, 2]<- length(count.fields(paste('./eeg/rt_cue/RT', i, '_DPX.txt', 
+                            sep = ''))) - 1 # <- first line is header
+  
+}; rm(i)
+
+# ----- Save data
+write.table(temp, './meta/n_segments.txt')
+
+
+
+
+
