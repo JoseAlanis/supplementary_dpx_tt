35500d638e677e1879571c46ab89ebfebafc37f9
Jose C. Garcia Alanis <12409129+JoseAlanis@users.noreply.github.com>
2019-07-09 18:14:59 +0200

    update artifact detection

--- eeg_preprocessing/02_preprocessing_artifact_detection.py
+++ eeg_preprocessing/02_preprocessing_artifact_detection.py
@@ -1,80 +1,76 @@
-# --- Jose C. Garcia Alanis
+# --- jose C. garcia alanis
 # --- utf-8
-# --- Python 3.6.2
+# --- Python 3.7.3 / mne 0.18.1
 #
-# --- EEG prepossessing - DPX TT
-# --- Version May 2019
+# --- eeg pre-processing for DPX TT
+# --- version: june 2019
 #
-# --- import data, drop "flat channels",
-# --- extract task blocks
+# --- detect and annotate artifact distorted segments in continuous data,
+# --- average reference
 
 # ========================================================================
 # ------------------- import relevant extensions -------------------------
-import glob
-import os
+import os.path as op
+from os import mkdir
+from glob import glob
 
-import re
+from re import findall
 import numpy as np
 import pandas as pd
 
-import mne
+from mne.io import read_raw_fif
+from mne import pick_types, find_events, Annotations
 
 # ========================================================================
-# --- global settings
-# --- prompt user to set project path
 # --- global settings
 # --- prompt user to set project path
 root_path = input("Type path to project directory: ")
 
 # look for directory
-if os.path.isdir(root_path):
+if op.isdir(root_path):
     print("Setting 'root_path' to ", root_path)
 else:
     raise NameError('Directory not found!')
 
+# derivatives path
+derivatives_path = op.join(root_path, 'derivatives')
+
 # path to eeg files
-data_path = os.path.join(root_path, 'derivatives/extract_blocks')
-# path for output
-derivatives_path = os.path.join(root_path, 'derivatives')
+data_path = op.join(derivatives_path, 'extract_blocks')
 
-# create directory for save
-if not os.path.isdir(os.path.join(derivatives_path, 'artifact_detection')):
-    os.mkdir(os.path.join(derivatives_path, 'artifact_detection'))
+# create directory for output
+if not op.isdir(op.join(derivatives_path, 'artifact_detection')):
+    mkdir(op.join(derivatives_path, 'artifact_detection'))
 
-output_path = os.path.join(derivatives_path, 'artifact_detection')
+# path for saving output
+output_path = op.join(derivatives_path, 'artifact_detection')
 
 # files to be analysed
-files = glob.glob(os.path.join(data_path, 'sub-*', '*-raw.fif'))
-
-# ========================================================================
-# -- define further variables that apply to all files in the data set
-# from MNE-Python (https://github.com/mne-tools/mne-python/blob/master/mne/preprocessing/bads.py);  # noqa
-# some adjustments were made.
-
+files = sorted(glob(op.join(data_path, 'sub-*', '*-raw.fif')))
 
 # ========================================================================
 # ------------ loop through files and extract blocks  --------------------
 for file in files:
 
     # --- 1) set up paths and file names -----------------------
-    filepath, filename = os.path.split(file)
+    filepath, filename = op.split(file)
     # subject in question
-    subj = re.match('sub-[0-9][0-9]', filename).group(0)
+    subj = findall(r'\d+', filename)[0].rjust(3, '0')
 
     # --- 2) import the preprocessed data ----------------------
-    raw = mne.io.read_raw_fif(file, preload=True)
+    raw = read_raw_fif(file, preload=True)
 
     # index of eogs and stim channels
-    picks_no_eeg = mne.pick_types(raw.info,
-                                  eeg=False,
-                                  eog=True,
-                                  stim=True)
+    picks_no_eeg = pick_types(raw.info,
+                              eeg=False,
+                              eog=True,
+                              stim=True)
 
     # channels which are of type "eeg"
-    picks_eeg = mne.pick_types(raw.info,
-                               eeg=True,
-                               eog=False,
-                               stim=False)
+    picks_eeg = pick_types(raw.info,
+                           eeg=True,
+                           eog=False,
+                           stim=False)
 
     # channel names
     channels = raw.info['ch_names']
@@ -83,10 +79,10 @@ for file in files:
     sfreq = raw.info['sfreq']
 
     # --- 3) get events in dataset ----------------------------
-    events = mne.find_events(raw,
-                             stim_channel='Status',
-                             output='onset',
-                             min_duration=0.002)
+    events = find_events(raw,
+                         stim_channel='Status',
+                         output='onset',
+                         min_duration=0.002)
 
     # --- 4) mark bad channels and segments --------------------
     # channels that should be ignored during the artifact detection procedure
@@ -110,7 +106,7 @@ for file in files:
     data = raw_copy.get_data(picks_eeg)
 
     # channels to be checked by artifact detection procedure
-    ch_ix = [raw_copy.info['ch_names'].index(chan) for chan in raw_copy.info['ch_names'] if chan not in ignore_ch]  # noqa
+    ch_ix = [channels.index(chan) for chan in channels if chan not in ignore_ch]
 
     # detect artifacts (i.e., absolute amplitude > 500 microV)
     times = []
@@ -128,7 +124,7 @@ for file in files:
         peak = []
         for channel in ch_ix:
             peak.append(abs(data[channel][sample]))
-        if max(peak) >= 400e-6:
+        if max(peak) >= 300e-6:
             times.append(float(sample))
             annotated_channels.append(channels[ch_ix[int(np.argmax(peak))]])
     # If artifact found create annotations for raw data
@@ -147,25 +143,40 @@ for file in files:
         for i in range(0, len(list(raw_copy.annotations.onset))):
             onsets.append(list(raw_copy.annotations.onset)[i])
         # Create new annotation info
-        annotations = mne.Annotations(onsets, duration, labels)
+        annotations = Annotations(onsets, duration, labels)
         raw_copy.set_annotations(annotations)
 
+    # save total annotated time
+    total_time = sum(duration)
     # save frequency of annotation per channel
-    frequency_of_annotation = {x: annotated_channels.count(x) for x in annotated_channels}  # noqa
+    frequency_of_annotation = {x: annotated_channels.count(x)*2 for x in annotated_channels}  # noqa
     # if exceeds 0.9% of total time --> mark as bad channel
-    threshold = (raw_copy._last_time - raw_copy._first_time) * .009
+    threshold = raw_copy.times[-1] * .01
+
     # save bads in info structure
     bad_chans = [chan for chan, value in frequency_of_annotation.items() if value >= int(threshold)]  # noqa
-    raw.info['bads'] = bad_chans
+    raw_copy.info['bads'] = bad_chans
+
+    # --- 4.3) plot data and check for inconsistencies  ----------
+    raw_copy.plot(scalings=dict(eeg=50e-6),
+                  n_channels=len(raw.info['ch_names']),
+                  bad_color='red',
+                  block=True)
+
+    # save bad channels for summary
+    interpolated = raw_copy.info['bads'].copy()
 
     # --- if bad channels were found, repeat preprocessing ---------
     if bad_chans:
-        # copy the file
+        # re-run artifact detection
         raw_copy = raw.copy()
+        raw_copy.info['bads'] = bad_chans
+
         # interpolate bads
         raw_copy.interpolate_bads(reset_bads=True,
                                   verbose=False,
                                   mode='accurate')
+
         # apply filter
         raw_copy.filter(l_freq=0.1, h_freq=50, picks=['eeg', 'eog'],
                         filter_length='auto',
@@ -178,8 +189,8 @@ for file in files:
         data = raw_copy.get_data(picks_eeg)
 
         # channels to be checked by artifact detection procedure
-        ch_ix = [raw_copy.info['ch_names'].index(chan) for chan in
-                 raw_copy.info['ch_names'] if chan not in ignore_ch]
+        ch_ix = [channels.index(chan) for chan in channels
+                 if chan not in ignore_ch]
 
         # detect artifacts (i.e., absolute amplitude > 500 microV)
         times = []
@@ -197,7 +208,7 @@ for file in files:
             peak = []
             for channel in ch_ix:
                 peak.append(abs(data[channel][sample]))
-            if max(peak) >= 400e-6:
+            if max(peak) >= 300e-6:
                 times.append(float(sample))
                 annotated_channels.append(channels[ch_ix[int(np.argmax(peak))]])
         # If artifact found create annotations for raw data
@@ -216,7 +227,7 @@ for file in files:
             for i in range(0, len(list(raw_copy.annotations.onset))):
                 onsets.append(list(raw_copy.annotations.onset)[i])
             # Create new annotation info
-            annotations = mne.Annotations(onsets, duration, labels)
+            annotations = Annotations(onsets, duration, labels)
             raw_copy.set_annotations(annotations)
 
     # --- 5) plot data and check for inconsistencies  ----------
@@ -231,26 +242,27 @@ for file in files:
 
     # --- 7) save segmented data  -----------------------------
     # create directory for save
-    if not os.path.exists(os.path.join(output_path, subj)):
-        os.mkdir(os.path.join(output_path, subj))
+    if not op.exists(op.join(output_path, 'sub-%s' % subj)):
+        mkdir(op.join(output_path, 'sub-%s' % subj))
 
     # save file
-    raw_copy.save(os.path.join(output_path, subj, '%s_artifact_detection-raw.fif' % subj),  # noqa
+    raw_copy.save(op.join(output_path, 'sub-' + str(subj),
+                          'sub-%s_artifact_detection-raw.fif' % subj),  # noqa
                   overwrite=True)
 
     # write summary
-    name = '%s_artifact_detection' % subj
-    sfile = open(os.path.join(output_path, subj, '%s.txt' % name), 'w')
+    name = 'sub-%s_artifact_detection.txt' % subj
+    sfile = open(op.join(output_path, 'sub-%s', name) % subj, 'w')
     # channels info
-    sfile.write('Channels interpolated:\n')
-    for ch in bad_chans:
+    sfile.write('Channels_interpolated:\n')
+    for ch in interpolated:
         sfile.write('%s\n' % ch)
     # frequency of annotation
-    sfile.write('Frequency of annotation:\n')
+    sfile.write('Frequency_of_annotation:\n')
     for ch, f in frequency_of_annotation.items():
         sfile.write('%s, %f\n' % (ch, f))
-    sfile.write('total annotated:\n')
-    sfile.write(str(round(sum(duration) / (raw_copy._last_time - raw_copy._first_time), 3)) + ' %\n')  # noqa
+    sfile.write('total_annotated:\n')
+    sfile.write(str(round(total_time / raw_copy.times[-1], 3) * 100) + ' %\n')
     sfile.close()
 
     del raw, raw_copy
