04687ab7efa9caee9483108d77ec16ef45ed14b0
Jose C. Garcia Alanis <12409129+JoseAlanis@users.noreply.github.com>
2020-03-12 19:37:21 +0100

    add block extraction

--- 01_task_blocks.py
+++ 01_task_blocks.py
@@ -10,12 +10,13 @@ Authors: José C. García Alanis <alanis.jcg@gmail.com>
 
 License: BSD (3-clause)
 """
+import os.path as op
+
 from mne.io import read_raw_fif
-from mne import find_events, Annotations, events_from_annotations, \
-    concatenate_raws
+from mne import events_from_annotations, concatenate_raws, open_report
 
 # All parameters are defined in config.py
-from config import fname, parser
+from config import fname, n_jobs, parser
 
 # Handle command line arguments
 args = parser.parse_args()
@@ -70,13 +71,14 @@ latencies = cue_evs[:, 0] / raw.info['sfreq']
 diffs = [(y - x) for x, y in zip(latencies, latencies[1:])]
 
 # get first event after a long break (i.e., when the time difference between
-# stimuli is greater than 10 seconds). This should only be the case in betweenn
+# stimuli is greater than 10 seconds). This should only be the case in between
 # task blocks.
 breaks = [diff for diff in range(len(diffs)) if diffs[diff] > 10]
 print('\n Identified breaks at positions', breaks)
 
-# --- 7) save start and end points of task blocks  ---------
-# subject '041' has more practice trials
+###############################################################################
+# 3) Save start and end points of task blocks
+# subject '041' has more practice trials (two rounds)
 if subject == 41:
     # start of first block
     b1s = latencies[breaks[2] + 1] - 2
@@ -103,108 +105,46 @@ else:
     else:
         b2e = latencies[-1] + 6
 
-# --- 8) extract block data --------------------------------
-# Block 1
+###############################################################################
+# 4) extract data belonging to the task blocks an concatenate
+# block 1
 raw_bl1 = raw.copy().crop(tmin=b1s, tmax=b1e)
-# Block 2
+# block 2
 raw_bl2 = raw.copy().crop(tmin=b2s, tmax=b2e)
 
-# --- 9) concatenate data ----------------------------------
+# concatenate data
 raw_blocks = concatenate_raws([raw_bl1, raw_bl2])
 
-ref_raw_bl1 = raw_bl1.copy().set_eeg_reference(ref_channels='average', projection=True)
+###############################################################################
+# 5) remove drifts and line noise
+raw_blocks_filt = raw_blocks.copy().filter(l_freq=0.1, h_freq=40.,
+                                           picks=['eeg', 'eog'],
+                                           filter_length='auto',
+                                           l_trans_bandwidth='auto',
+                                           h_trans_bandwidth='auto',
+                                           method='fir',
+                                           phase='zero',
+                                           fir_window='hamming',
+                                           fir_design='firwin',
+                                           n_jobs=n_jobs)
 
-raw_bl1_filt = raw_bl1.copy().filter(l_freq=0.1, h_freq=40., picks=['eeg', 'eog'],
-                                     filter_length='auto',
-                                     l_trans_bandwidth='auto',
-                                     h_trans_bandwidth='auto',
-                                     method='fir',
-                                     phase='zero',
-                                     fir_window='hamming',
-                                     fir_design='firwin',
-                                     n_jobs=2)
+# plot power spectral density
+raw_psd = raw_blocks_filt.plot_psd(show=False)
 
-ref_raw_bl1.plot(scalings=dict(eeg=100e-6, eog=100e-6), n_channels=len(raw.info['ch_names']))
-raw_bl1.plot(scalings=dict(eeg=100e-6, eog=100e-6), n_channels=len(raw.info['ch_names']))
-raw_bl1_filt.plot(scalings=dict(eeg=100e-6, eog=100e-6), n_channels=len(raw.info['ch_names']))
-
-# --- 10) lower the sample rate  ---------------------------
-raw_blocks.resample(sfreq=256.)
-
-# --- 11) extract events and save them in annotations ------
-annot_infos = ['onset', 'duration', 'description']
-annotations = pd.DataFrame(raw_blocks.annotations)
-annotations = annotations[annot_infos]
-
-# path to events .tsv
-events = find_events(raw_blocks,
-                     stim_channel='Status',
-                     output='onset',
-                     min_duration=0.002)
-# import events
-events = pd.DataFrame(events, columns=annot_infos)
-events.onset = events.onset / raw_blocks.info['sfreq']
-
-# merge with annotations
-events = events.append(annotations, ignore_index=True)
-# sort by onset
-events = events.sort_values(by=['onset'])
-
-# crate annotations object
-annotations = Annotations(events['onset'],
-                          events['duration'],
-                          events['description'],
-                          orig_time=date_of_record)
-# apply to raw data
-raw_blocks.set_annotations(annotations)
-
-# drop stimulus channel
-raw_blocks.drop_channels('Status')
-
-raw_blocks.plot(n_channels=len(raw_blocks.ch_names),
-                scalings=dict(eeg=100e-6),
-                block=True)
-
-# --- 12) save segmented data  -----------------------------
-# create directory for save
-if not op.exists(op.join(output_path, 'sub-%s' % subj)):
-    mkdir(op.join(output_path, 'sub-%s' % subj))
+###############################################################################
+# 6) export data to .fif for further processing
+# output path
+output_path = fname.output(processing_step='task_blocks',
+                           subject=subject,
+                           file_type='raw.fif')
 
 # save file
-raw_blocks.save(op.join(output_path, 'sub-' + str(subj),
-                        'sub-%s_task_blocks-raw.fif' % subj),
-                overwrite=True)
+raw.save(output_path, overwrite=True)
 
-# --- 13) save script summary  ------------------------------
-# get cue events in segmented data
-events = events_from_annotations(raw_blocks, regexp='^[7][0-5]')[0]
-
-# number of trials
-nr_trials = len(events)
-
-# write summary
-name = 'sub-%s_task_blocks_summary.txt' % subj
-sfile = open(op.join(output_path, 'sub-%s', name) % subj, 'w')
-#     # block info
-sfile.write('Block_1_from:\n%s to %s\n' % (str(round(b1s, 2)),
-                                           str(round(b1e, 2))))
-sfile.write('Block_2_from:\n%s to %s\n' % (str(round(b2s, 2)),
-                                           str(round(b2e, 2))))
-sfile.write('Block_1_length:\n%s\n' % round(b1e - b1s, 2))
-sfile.write('Block_2_length:\n%s\n' % round(b2e - b2s, 2))
-# number of trials in file
-sfile.write('number_of_trials_found:\n%s\n' % nr_trials)
-# channels dropped
-sfile.write('channels_with_zero_variance:\n')
-for ch in flats:
-    sfile.write('%s\n' % ch)
-sfile.close()
-
-
-
-
-# block durations
-print('Block 1 from', round(b1s, 3), 'to', round(b1e, 3), '\nBlock length ',
-      round(b1e - b1s, 3))
-print('Block 2 from', round(b2s, 3), 'to', round(b2e, 3), '\nBlock length ',
-      round(b2e - b2s, 3))
+###############################################################################
+# 7) create HTML report
+with open_report(fname.report(subject=subject)[0]) as report:
+    report.add_figs_to_section(raw_psd, 'Blocks PSD', section='Filtered Data',
+                               replace=True)
+    report.save(fname.report(subject=subject)[1], overwrite=True,
+                open_browser=False)
