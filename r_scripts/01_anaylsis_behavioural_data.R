cd120c4ec9b94b94a31e09c89021e8c1585fe994
Jose C. Garcia Alanis <12409129+JoseAlanis@users.noreply.github.com>
2020-08-17 20:38:51 +0200

    update behavioral analyses

--- r_scripts/01_anaylsis_behavioural_data.R
+++ r_scripts/01_anaylsis_behavioural_data.R
@@ -9,7 +9,7 @@ source('./r_functions/getPacks.R')
 source('./r_functions/spr2.R')
 
 
-# 1) define the path to behavioral data directory ------------------------------
+# 1) define the path to behavioral data directory -----------------------------
 
 # get system and user information
 host <- Sys.info()
@@ -27,7 +27,7 @@ if (host['nodename'] == "josealanis-desktop") {
 }
 
 
-# 2) import in the data --------------------------------------------------------
+# 2) import in the data -------------------------------------------------------
 # this part requires the package 'dplyr'
 getPacks('dplyr')
 
@@ -40,11 +40,12 @@ rt_list <- lapply(rt_files, read.table, sep = '\t', header = T)
 # put the in a data frame
 rt_df <- bind_rows(rt_list, .id = "column_label")
 
-# # clean up if desired
-# rm(rt_list, rt_files)
+# recode block variable
+rt_df <- rt_df %>% mutate(block =  ifelse(block == 0, 1, 2)) %>%
+  mutate(block = factor(block, labels = c('Block 1', 'Block 2')))
 
 
-# 3) exploratory analyses ------------------------------------------------------
+# 3) exploratory analyses correct reactions -----------------------------------
 
 # extract trials with correct reponses
 corrects <- rt_df %>%
@@ -82,68 +83,77 @@ corrects %>%
   group_by(cue) %>%
   summarise(mn = mean(n), sd = sd(n))
 
-# plot distribution of reaction time
-getPacks(c('ggplot2', 'viridis'))
-pd <- position_dodge(1.15)
-rt_plot <- ggplot(corrects,
-                  aes(x = w_rt, fill = probe, colour = probe)) +
-  geom_density(alpha = 0.25) +
-  # facet_wrap(~ probe, scales = 'free', nrow = 4) +
-  labs(x = 'RT (sec)',
-       y = 'Density',
-       fill = 'Cue-Probe',
-       colour = 'Cue-Probe') +
-  scale_y_continuous(limits = c(0.0, 8.75),
-                     breaks = seq(0.0, 8.0, 2.0)) +
-  scale_x_continuous(limits = c(0.0, 0.8)) +
-  scale_fill_viridis(discrete = T) +
-  scale_color_viridis(discrete = T) +
-  geom_segment(aes(x = -Inf, y = 0.0, xend = -Inf, yend = 8.0),
+# *** plot distribution of reaction time ***
+getPacks(c('ggplot2', 'viridis', 'Hmisc'))
+
+# data for plot
+m_rt <- corrects %>%
+  group_by(subject, probe, block) %>%
+  summarise(m = mean(rt))
+
+# add some space between geoms
+pjd <- position_jitterdodge(
+  jitter.width = 0.5,
+  jitter.height = 0,
+  dodge.width = 0,
+  seed = 2)
+pn <- position_nudge(x = 0.25)
+
+# create the plot
+rt_plot <- ggplot(data = m_rt,
+                  aes(x = block, y = m,
+                      fill = probe, shape = block)) +
+  facet_wrap(~ probe, ncol = 4, scales = 'free_y') +
+  geom_line(aes(group = subject), position = pjd, alpha = 0.25, size = 0.33,
+            color='black') +
+  geom_jitter(position = pjd, size = 1.75, color='black', alpha = 0.25) +
+  stat_summary(fun.data = "mean_cl_boot", position = pn,
+               fun.args = list(B = 10000, conf.int = 0.99), size = 1) +
+  scale_fill_viridis(discrete = T, end = .95) +
+  scale_shape_manual(values = c(21, 23), guide = FALSE) +
+  labs(x = 'Cue-Probe',
+       y = 'RT (sec)',
+       color = F) +
+  guides(fill = guide_legend(override.aes = list(shape = 21))) +
+  scale_y_continuous(limits = c(0.1, 0.7),
+                     breaks = seq(0.1, 0.7, 0.1)) +
+  geom_segment(aes(x = -Inf, y = 0.1, xend = -Inf, yend = 0.7),
                color = 'black', size = rel(0.5), linetype = 1) +
-  geom_segment(aes(x = 0.0, y = -Inf, xend = 0.8, yend = -Inf),
+  geom_segment(aes(x = 'Block 1', y = -Inf, xend = 'Block 2', yend = -Inf),
                color = 'black', size = rel(0.5), linetype = 1) +
   theme(axis.title.x = element_text(color = 'black', size = 12,
                                     margin = margin(t = 10)),
         axis.title.y= element_text(color = 'black', size = 12,
                                    margin = margin(r = 10)),
         axis.text = element_text(color = 'black', size = 10),
-        panel.background = element_rect(fill = 'gray95'),
+        panel.background = element_rect(fill = 'gray97'),
         strip.text = element_blank(),
         strip.background = element_blank(),
         legend.position='bottom',
-        legend.title = element_text(size = 10, face = 'bold'),
+        legend.title = element_blank(),
         legend.text = element_text(size = 10),
-        panel.spacing = unit(1, "lines")) +
-  # geom_density(data = corrects, aes(x = w_rt),
-  #              alpha = 0.0, color = 'black', linetype = 1) +
-  geom_boxplot(data = corrects,
-               aes(rt, y = 8.0),
-               color = 'black',
-               width = 0.75,
-               outlier.size = 0.25,
-               alpha = 0.5, position = pd,
-               show.legend = FALSE); rt_plot
+        panel.spacing = unit(1, "lines")); rt_plot
+# save to disk
 ggsave(filename = '../data/derivatives/results/figures/rt_distribution.pdf',
-       plot = rt_plot, width = 5, height = 4)
+       plot = rt_plot, width = 7.5, height = 5, dpi = 300)
 
 
-# 4) model rt data -------------------------------------------------------------
-
+# 4) model rt of correct reactions --------------------------------------------
 # this part requires the following packages
 getPacks(c('lme4', 'car',
            'sjPlot',
            'tidyr'))
 
 # summarise to the level of trial types by block
-corrects %>%
+corrects <- corrects %>%
   group_by(subject, block, probe) %>%
   summarise(m_rt = mean(w_rt))
 
 # transform variables to factors
-dat_for_mod$probe <- as.factor(dat_for_mod$probe)
-dat_for_mod$block <- as.factor(dat_for_mod$block)
+corrects$probe <- as.factor(corrects$probe)
+corrects$block <- as.factor(corrects$block)
 
-rt_mod <- lmer(data = dat_for_mod,
+rt_mod <- lmer(data = corrects,
                m_rt ~ block * probe + (1 |subject),
                contrasts = list(probe = 'contr.sum',
                                 block = 'contr.sum'))
@@ -169,13 +179,14 @@ tab_df(title = 'Anova RT Model',
              as.data.frame(rt_anova),
              c(NA, spr2)), digits = 3)
 
+
 # 5) interaction analysis for rt data -----------------------------------------
 # this section requires the following packages
 getPacks(c('emmeans', 'ggplot2', 'dplyr',  'tidyr'))
 
 # levels of the probe variable by block (e.g. AX in block 0 = AX 0)
-probe_levels <- c('AX 0', 'AY 0', 'BX 0', 'BY 0',
-                  'AX 1', 'AY 1', 'BX 1', 'BY 1')
+probe_levels <- c('AX Block 1', 'AY Block 1', 'BX Block 1', 'BY Block 1',
+                  'AX Block 2', 'AY Block 2', 'BX Block 2', 'BY Block 2')
 
 # compute estimated marginal means
 rt_means <- emmeans(rt_mod, ~ probe * block)
@@ -207,6 +218,7 @@ es_probes <- eff_size(probe_means, sigma = totSD, edf = edf); es_probes
 block_means <- emmeans(rt_mod, ~ block)
 es_block <- eff_size(block_means, sigma = totSD, edf = edf); es_block
 
+# compute effect sizes
 es <- eff_size(rt_means, sigma = totSD, edf = edf)
 es <- as.data.frame(es)
 es <- es %>%
@@ -217,6 +229,14 @@ es <- es %>%
 es$var1 <- factor(es$var1, levels = probe_levels)
 es$var2 <- factor(es$var2, levels = probe_levels)
 
+# adjust p-values and add "*" for significance coding
+pairwise_rt <- as.data.frame(pairs(rt_means, adjust='bonferroni'))
+es$p_val <- pairwise_rt$p.value
+es <- es %>%
+  mutate(sig = ifelse(p_val > 0.05, ' ',
+                      ifelse(p_val > 0.01, '*',
+                             ifelse(p_val > 0.001, '**', '***'))))
+
 # create the plot
 pw_rt_plot <- ggplot(rt_means_df, aes(var1, var2)) +
   geom_tile(fill = 'white') +
@@ -225,27 +245,260 @@ pw_rt_plot <- ggplot(rt_means_df, aes(var1, var2)) +
   geom_text(aes(label = paste(round(lower.CL, 2),
                               round(upper.CL, 2),
                               sep = ' - ')),
-            vjust = 2.5, size = 3.25) +
+            vjust = 2.5, size = 2.75) +
   geom_tile(data = es, aes(var1, var2, fill=effect.size),
             colour = 'white', size = 0.8) +
+  geom_text(data = es, aes(var1, var2, label = sig),
+            colour = 'black', size = 7,  vjust = 1) +
   # scale_fill_viridis(option = 'B', limits = c(-0.5, 0.5)) +
-  scale_fill_distiller(palette = "RdBu", limits = c(-5.0, 5.0)) +
-  geom_segment(aes(x = -Inf, y = 'AX 0', xend = -Inf, yend = 'BY 1'),
+  scale_fill_distiller(palette = "RdBu", limits = c(-4.5, 4.5),
+                       breaks = seq(-4.0, 4.0, 2.0)) +
+  geom_segment(aes(x = -Inf, y = 'AX Block 1',
+                   xend = -Inf, yend = 'BY Block 2'),
                color = 'black', size = rel(0.5), linetype = 1) +
-  geom_segment(aes(x = 'AX 0', y = -Inf, xend = 'BY 1', yend = -Inf),
+  geom_segment(aes(x = 'AX Block 1', y = -Inf,
+                   xend = 'BY Block 2', yend = -Inf),
                color = 'black', size = rel(0.5), linetype = 1) +
   labs(x = 'Trial type by block', y = 'Trial type by block',
        fill = "Difference (Cohen's D)") +
-  theme(axis.title.x = element_text(color = 'black', size = 15,
+  theme(axis.title.x = element_text(color = 'black', size = 16,
                                     margin = margin(t = 10)),
-        axis.title.y= element_text(color = 'black', size = 15,
+        axis.title.y= element_text(color = 'black', size = 16,
                                    margin = margin(r = 10)),
-        axis.text = element_text(color = 'black', size = 12),
-        panel.background = element_rect(fill = 'gray95'),
+        axis.text.y = element_text(color = 'black', size = 14),
+        axis.text.x = element_text(color = 'black', size = 14, angle = 45,
+                                   hjust = 1),
+        panel.background = element_rect(fill = 'gray97'),
         strip.text = element_blank(),
         strip.background = element_blank(),
         legend.position='bottom',
         panel.spacing = unit(1, "lines")) +
-  coord_flip()
+  coord_flip(); pw_rt_plot
+# save to disk
 ggsave(filename = '../data/derivatives/results/figures/pairwise_rt_plot.pdf',
        plot = pw_rt_plot, width = 6.5, height = 7)
+
+
+# 6) exploratory analysis of error rates --------------------------------------
+getPacks('dplyr')
+
+# compute number of trials per condition
+total <- rt_df %>%
+  mutate(probe =
+           ifelse(nchar(probe) > 1, substr(probe, 2, 2), probe),
+         trial_type = paste0(cue, probe)) %>%
+  group_by(subject, block, trial_type) %>%
+  mutate(n_trials = sum(!is.na(trial))) %>%
+  select(subject, block, trial_type, n_trials) %>%
+  arrange(subject, block, trial_type) %>%
+  unique()
+
+# compute number of errors per condition
+errors <- rt_df %>%
+  mutate(probe =
+           ifelse(nchar(probe) > 1, substr(probe, 2, 2), probe),
+         trial_type = paste0(cue, probe)) %>%
+  filter(reaction_probes == 'Incorrect') %>%
+  group_by(subject, block, trial_type) %>%
+  mutate(n_errors = sum(!is.na(trial))) %>%
+  summarise(n_errors = mean(n_errors)) %>%
+  arrange(subject, block, trial_type)
+
+# merge data frames
+errors <- merge(total, errors, c('subject', 'block', 'trial_type'), all.x = T)
+# replace missing values with zeros
+errors[is.na(errors)] <- 0
+
+# compute error rates
+errors <- errors %>%
+  mutate(error_rate=(n_errors+0.5)/(n_trials+1))
+
+errors %>% group_by(trial_type) %>%
+  summarise(m = mean(error_rate), sd = sd(error_rate))
+
+errors %>% group_by(block, trial_type) %>%
+  summarise(m = mean(error_rate), sd = sd(error_rate))
+
+# plot distribution of error rates
+getPacks(c('ggplot2', 'viridis'))
+pjd <- position_jitterdodge(
+  jitter.width = 0.5,
+  jitter.height = 0,
+  dodge.width = 0,
+  seed = 2)
+pn <- position_nudge(x = 0.25)
+errors_plot <- ggplot(errors,
+                  aes(y = error_rate, x = block,
+                      fill = trial_type, shape = block)) +
+  facet_wrap(~ trial_type, ncol = 4, scales = 'free_y') +
+  geom_line(aes(group = subject), position = pjd, alpha = 0.25, size = 0.33,
+            color='black') +
+  geom_jitter(position = pjd, size = 1.75, color='black', alpha = 0.2) +
+  stat_summary(fun.data = "mean_cl_boot", position = pn,
+               fun.args = list(B = 10000, conf.int = 0.99), size = 1) +
+  scale_fill_viridis(discrete = T, end = .95) +
+  scale_shape_manual(values = c(21, 23), guide = FALSE) +
+  labs(x = 'Cue-Probe',
+       y = 'Error rate',
+       color = F) +
+  guides(fill = guide_legend(override.aes = list(shape = 21))) +
+  scale_y_continuous(limits = c(0.0, 0.9),
+                     breaks = seq(-0.0, 0.9, 0.1)) +
+  geom_segment(aes(x = -Inf, y = 0.0, xend = -Inf, yend = 0.9),
+               color = 'black', size = rel(0.5), linetype = 1) +
+  geom_segment(aes(x = 'Block 1', y = -Inf, xend = 'Block 2', yend = -Inf),
+               color = 'black', size = rel(0.5), linetype = 1) +
+  theme(axis.title.x = element_text(color = 'black', size = 12,
+                                    margin = margin(t = 10)),
+        axis.title.y= element_text(color = 'black', size = 12,
+                                   margin = margin(r = 10)),
+        axis.text = element_text(color = 'black', size = 10),
+        panel.background = element_rect(fill = 'gray97'),
+        strip.text = element_blank(),
+        strip.background = element_blank(),
+        legend.position='bottom',
+        legend.title = element_blank(),
+        legend.text = element_text(size = 10),
+        panel.spacing = unit(1, "lines")); errors_plot
+# save to disk
+ggsave(filename = '../data/derivatives/results/figures/errors_distribution.pdf',
+       plot = errors_plot, width = 7.5, height = 5, dpi = 300)
+
+
+# 7) fit model for error rates ------------------------------------------------
+# this part requires the following packages
+getPacks(c('lme4', 'car',
+           'sjPlot',
+           'tidyr'))
+
+# transform variables to factors
+errors$trial_type <- as.factor(errors$trial_type)
+errors$block <- as.factor(errors$block)
+
+errors_mod <- lmer(data = errors,
+               log(error_rate) ~ block * trial_type + (1|subject),
+                   contrasts = list(trial_type = 'contr.sum',
+                                    block = 'contr.sum'))
+# anova for model
+errors_anova <- car::Anova(errors_mod, test = 'F', type = 'III')
+# semi-partial R-squared for predictors
+err_spr2 <- spR2(errors_anova)
+
+# create a teble of the model summary
+tab_model(errors_mod, digits = 3,
+          file = '../data/derivatives/results/tables/errors_model.html',
+          pred.labels = c('(Intercept)',
+                          'Block 1',
+                          'AX', 'AY', 'BX',
+                          'Block 1 * AX',
+                          'Block 1 * AY',
+                          'Block 1 * BX'))
+
+# create a teble of the model anova
+tab_df(title = 'Anova Errors Model',
+       file = '../data/derivatives/results/tables/anova_errors_mod.html',
+       cbind(row.names(errors_anova),
+             as.data.frame(errors_anova),
+             c(NA, err_spr2)), digits = 3)
+
+
+# 8) interaction analysis for errors data -------------------------------------
+# this section requires the following packages
+getPacks(c('emmeans', 'ggplot2', 'dplyr',  'tidyr'))
+
+# levels of the probe variable by block (e.g. AX in block 0 = AX 0)
+probe_levels <- c('AX Block 1', 'AY Block 1', 'BX Block 1', 'BY Block 1',
+                  'AX Block 2', 'AY Block 2', 'BX Block 2', 'BY Block 2')
+
+# compute estimated marginal means
+error_means <- emmeans(errors_mod, ~ trial_type * block, type = 'response')
+# compute significance of pairwise contrasts
+pwpm(error_means, adjust = 'mvt', flip = TRUE)
+
+# prepare pairwise data for plot
+error_means_df <- as.data.frame(error_means)
+error_means_df <- error_means_df %>%
+  arrange(block) %>%
+  mutate(var1 = paste(trial_type, block, sep = ' '),
+         var2 = paste(trial_type, block, sep = ' '))
+
+# reorder levels
+error_means_df$var1 <- factor(error_means_df$var1,
+                              levels = probe_levels)
+error_means_df$var2 <- factor(error_means_df$var2,
+                              levels = probe_levels)
+
+# model variance estimates and residual degrees of freedom
+err_mod_var <- VarCorr(errors_mod)
+err_totSD <- sqrt(sum(as.data.frame(err_mod_var)$vcov))
+err_edf <- df.residual(errors_mod)
+
+# compute effect sizes for the pairwise contrasts of cue-probe combination
+probe_error_means <- emmeans(errors_mod, ~ trial_type)
+es_error_probes <- eff_size(probe_error_means, sigma = err_totSD, edf = err_edf)
+es_error_probes
+
+# compute effect sizes for the pairwise contrasts of block
+block_error_means <- emmeans(errors_mod, ~ block)
+es_error_block <- eff_size(block_error_means, sigma = err_totSD, edf = err_edf)
+es_error_block
+
+# effect sizes for plot
+es_error <- eff_size(error_means, sigma = err_totSD, edf = err_edf)
+es_error <- as.data.frame(es_error)
+es_error <- es_error %>%
+  separate(contrast, into = c('var1', 'var2'), sep = ' - ') %>%
+  mutate(var1 = gsub(var1, pattern = ',', replacement = ' '),
+         var2 = gsub(var2, pattern = ',', replacement = ' '))
+# reorder levels
+es_error$var1 <- factor(es_error$var1, levels = probe_levels)
+es_error$var2 <- factor(es_error$var2, levels = probe_levels)
+
+# adjust p-values and add "*" for coding significance
+pairwise_err <- as.data.frame(pairs(error_means, adjust='bonferroni'))
+es_error$p_val <- pairwise_err$p.value
+es_error <- es_error %>%
+  mutate(sig = ifelse(p_val < 0.001, '***',
+                     ifelse(p_val < 0.01, '**',
+                            ifelse(p_val < 0.05, '*', ' '))))
+
+# create the plot
+pw_error_plot <- ggplot(error_means_df, aes(var1, var2)) +
+  geom_tile(fill = 'white') +
+  geom_text(aes(label = round(response, 2)), size = 5,
+            vjust = -.05) +
+  geom_text(aes(label = paste(round(lower.CL, 2),
+                              round(upper.CL, 2),
+                              sep = ' - ')),
+            vjust = 2.5, size = 2.75) +
+  geom_tile(data = es_error, aes(var1, var2, fill=effect.size),
+            colour = 'white', size = 0.8) +
+  geom_text(data = es_error, aes(var1, var2, label = sig),
+            colour = 'black', size = 7,  vjust = 1) +
+  # scale_fill_viridis(option = 'B', limits = c(-0.5, 0.5)) +
+  scale_fill_distiller(palette = "RdBu", limits = c(-4.5, 4.5),
+                       breaks = seq(-4.0, 4.0, 2.0)) +
+  geom_segment(aes(x = -Inf, y = 'AX Block 1',
+                   xend = -Inf, yend = 'BY Block 2'),
+               color = 'black', size = rel(0.5), linetype = 1) +
+  geom_segment(aes(x = 'AX Block 1', y = -Inf,
+                   xend = 'BY Block 2', yend = -Inf),
+               color = 'black', size = rel(0.5), linetype = 1) +
+  labs(x = 'Trial type by block', y = 'Trial type by block',
+       fill = "Difference (Cohen's D)") +
+  theme(axis.title.x = element_text(color = 'black', size = 16,
+                                    margin = margin(t = 10)),
+        axis.title.y= element_text(color = 'black', size = 16,
+                                   margin = margin(r = 10)),
+        axis.text.y = element_text(color = 'black', size = 14),
+        axis.text.x = element_text(color = 'black', size = 14, angle = 45,
+                                   hjust = 1),
+        panel.background = element_rect(fill = 'gray97'),
+        strip.text = element_blank(),
+        strip.background = element_blank(),
+        legend.position='bottom',
+        panel.spacing = unit(1, "lines")) +
+  coord_flip(); pw_error_plot
+# save to disk
+ggsave(filename = '../data/derivatives/results/figures/pairwise_error_plot.pdf',
+       plot = pw_error_plot, width = 6.5, height = 7, dpi = 300)
